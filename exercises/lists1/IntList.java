adc4cb8dcf8ee323ec475148e894aa00492b4874
Mengting Yao <yaomengting110@gmail.com>
2019-07-30 10:38:11 -0500

    IntList exercise1&2

--- exercises/lists1/IntList.java
+++ exercises/lists1/IntList.java
@@ -36,7 +36,14 @@ public class IntList {
     /** Returns an IntList identical to L, but with
       * each element incremented by x. Not allowed to use
       * the 'new' keyword. */
-   
+    public static IntList dincrList(IntList L, int x) {
+        while(L != null){
+			L.first -= x;
+			L = L.rest;
+		}
+		/* Your code here. */
+        return L;
+    }
       
     public static void main(String[] args) {
         IntList L = new IntList(5, null);
@@ -50,6 +57,7 @@ public class IntList {
         
        
         System.out.println(incrList(L, 3));
+		System.out.println(dincrList(L, 3));
                
     }
 
